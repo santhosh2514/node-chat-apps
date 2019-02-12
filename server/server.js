1b161488cbc1947e16234a76a47b17b788127bb8
santhosh2514 <46202799+santhosh2514@users.noreply.github.com>
2019-02-12 20:02:24 +0530

    Added Jquery for browser display

--- server/server.js
+++ server/server.js
@@ -21,9 +21,10 @@ io.on('connection',(socket)=>{
 
      socket.emit('newMessage',generateMessage('Admin','Welcome to the chat app'));
      socket.broadcast.emit('newMessage',generateMessage('Admin','New user joined'));
-   socket.on('createMessage',(message)=>{
+   socket.on('createMessage',(message,callback)=>{
      console.log('createMessage',message);
      io.emit('newMessage',generateMessage(message.from,message.text))
+     callback();
      // socket.broadcast.emit('newMessage',{
      //   from:message.from,
      //   text:message.text,
